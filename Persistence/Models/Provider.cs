47ed7bd64642300a2db83dc8f406019f65e00dfb
Caleb J. Acosta <caleb.acosta.j@gmail.com>
2023-04-17 15:04:45 -0600

    User, Provider and Repo models and migrations

---
+++ Persistence/Models/Provider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mineshard.Persistence.Models
+{
+    public class Provider
+    {
+        public Guid id { get; set; }
+        public required string Name { get; set; }
+        public required string ProviderUrl { get; set; }
+    }
+}
