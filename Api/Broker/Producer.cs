59bb191b3c9eb690b32bcd1d3f91f44ea154a9e0
Caleb Acosta <caleb.acosta.j@gmail.com>
2023-04-24 17:44:38 -0600

    Broker producer

---
+++ Api/Broker/Producer.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+
+namespace Mineshard.Api.Broker
+{
+    public class Producer
+    {
+        private readonly ConnectionFactory _factory;
+        private readonly string _queueName;
+        private readonly string _routingKey;
+        private readonly string _exchangeName;
+
+        public Producer(IConfiguration config)
+        {
+            _exchangeName =
+                config.GetValue<string>("RabbitMQ:ExchangeName")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing ExchangeName configuration"
+                );
+            _queueName =
+                config.GetValue<string>("RabbitMQ:QueueName")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing QueueName configuration"
+                );
+            _routingKey =
+                config.GetValue<string>("RabbitMQ:RoutingKey")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing RoutingKey configuration"
+                );
+
+            var hostName =
+                config.GetValue<string>("RabbitMQ:HostName")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing HostName configuration"
+                );
+            _factory = new ConnectionFactory() { HostName = hostName };
+        }
+
+        public void Send(Guid repoId)
+        {
+            using (var connection = _factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Declare the Exchange
+
+                channel.ExchangeDeclare(exchange: _exchangeName, type: ExchangeType.Direct);
+
+                // Declare the Queue
+                channel.QueueDeclare(
+                    queue: _queueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                // Bind the queue to the exchange with the routing key
+                channel.QueueBind(
+                    queue: _queueName,
+                    exchange: _exchangeName,
+                    routingKey: _routingKey
+                );
+
+                var body = Encoding.UTF8.GetBytes(repoId.ToString());
+
+                // Publish the message
+                channel.BasicPublish(
+                    exchange: _exchangeName,
+                    routingKey: _routingKey,
+                    basicProperties: null,
+                    body: body
+                );
+            }
+        }
+    }
+}
