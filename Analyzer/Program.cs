1ac0a4f4653b145b831dbf9eafe80eda85c988c9
Caleb Acosta <caleb.acosta.j@gmail.com>
2023-04-27 14:54:25 -0600

    Workers pool

--- Analyzer/Program.cs
+++ Analyzer/Program.cs
@@ -1,13 +1,41 @@
 // Workaroud for https://github.com/dotnet/roslyn-analyzers/issues/6141
 #pragma warning disable CA1852
 
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Mineshard.Analyzer;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+var cwd = Directory.GetParent(Directory.GetCurrentDirectory());
 
-host.Run();
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(cwd == null ? Directory.GetCurrentDirectory() : cwd.FullName)
+    .AddJsonFile("appsettings.json");
+
+var configuration = builder.Build();
+
+if (configuration == null)
+    throw new InvalidDataException("Couldn't find configuration file");
+
+// Create 10 threads to listen messages
+for (int i = 1; i <= 10; i++)
+{
+    Thread workerThread = new Thread(new ParameterizedThreadStart(Setup));
+    workerThread.Start(configuration);
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+void Setup(object? config)
+{
+    // Log thread creation
+    Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} started.");
+    if (config is null)
+        return;
+    var worker = new Worker((IConfiguration) config);
+    worker.StartConsuming();
+}
--- Analyzer/Worker.cs
+++ Analyzer/Worker.cs
@@ -1,20 +1,74 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+using Mineshard.Analyzer.Controllers;
+using Mineshard.Analyzer.Core;
+using Mineshard.Persistence.Repos;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
 namespace Mineshard.Analyzer;
 
-public class Worker : BackgroundService
+public class Worker : IDisposable
 {
-    private readonly ILogger<Worker> logger;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    private string _hostName;
+    private string _queueName;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(IConfiguration config)
     {
-        this.logger = logger;
+        _hostName =
+                config.GetValue<string>("RabbitMQ:HostName")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing HostName configuration"
+                );
+
+        _queueName =
+                config.GetValue<string>("RabbitMQ:QueueName")
+                ?? throw new ArgumentNullException(
+                    nameof(config),
+                    "Missing QueueName configuration"
+                );
+
+        var factory = new ConnectionFactory() { HostName = _hostName };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: _queueName,
+                             durable: true,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public void StartConsuming()
     {
-        while (!stoppingToken.IsCancellationRequested)
+        // Start consuming messages from the queue
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
         {
-            logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+            // Get the message body as a string
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            // Print the message to the console
+            // Execute here the actual code
+            Console.WriteLine("Received message: {0}, from {1}", message, Environment.CurrentManagedThreadId);
+            Thread.Sleep(10_000);
+            Console.WriteLine("Finished message: {0}, from {1}", message, Environment.CurrentManagedThreadId);
+        };
+        _channel.BasicConsume(queue: _queueName,
+                                autoAck: true,
+                                consumer: consumer);
+    }
+
+    public void Dispose()
+    {
+        _channel.Dispose();
+        _connection.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
