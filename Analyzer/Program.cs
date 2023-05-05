e1244407dcc296668e9106acc0f3d375a4cf08f9
Caleb Acosta <caleb.acosta.j@gmail.com>
2023-05-05 17:41:20 -0600

    Removed unnecesary threads in main program

--- Analyzer/Program.cs
+++ Analyzer/Program.cs
@@ -25,21 +25,53 @@ var configuration = builder.Build();
 if (configuration == null)
     throw new InvalidDataException("Couldn't find configuration file");
 
+var cancellationTokenSource = new CancellationTokenSource();
+
+var userInputTask = Task.Run(() =>
+{
+    while (true)
+    {
+        if (
+            Console.ReadKey(true).Modifiers == ConsoleModifiers.Shift
+            && Console.ReadKey(true).Key == ConsoleKey.C
+        )
+        {
+            cancellationTokenSource.Cancel();
+            break;
+        }
+    }
+});
+
+var workers = new List<Worker>();
+
+var reporter = new ReportsController(
+    new ReportsDbRepo(),
+    new Reporter((IConfiguration)configuration)
+);
+
+var hostName =
+    configuration.GetValue<string>("RabbitMQ:HostName")
+    ?? throw new InvalidDataException("Missing HostName configuration");
+var queueName =
+    configuration.GetValue<string>("RabbitMQ:QueueName")
+    ?? throw new InvalidDataException("Missing QueueName configuration");
+
 // Create 10 threads to listen messages
 for (int i = 1; i <= 10; i++)
 {
-    Thread workerThread = new Thread(new ParameterizedThreadStart(Setup));
-    workerThread.Start(configuration);
+    var factory = new ConnectionFactory() { HostName = hostName };
+    var connection = factory.CreateConnection();
+    var worker = new Worker(queueName, reporter, connection);
+    workers.Add(worker);
 }
 
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
-
-static void Setup(object? config)
+foreach (var worker in workers)
 {
-    if (config is null)
-        return;
-    var reporter = new ReportsController(new ReportsDbRepo(), new Reporter((IConfiguration)config));
-    var worker = new Worker((IConfiguration)config, reporter);
     worker.StartConsuming();
 }
+
+Console.WriteLine("Listening inconming messages...");
+Console.WriteLine("Press Shift + C to exit...");
+
+// Wait for cancellation
+Task.WaitAny(userInputTask, Task.Delay(Timeout.Infinite, cancellationTokenSource.Token));
